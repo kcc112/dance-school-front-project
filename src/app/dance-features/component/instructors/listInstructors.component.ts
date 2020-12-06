8f18230dc2fea130aa36a13e7343491debe86a95
Ernest Blaz <216727@edu.p.lodz.pl>
2020-12-06 16:34:01 +0100

    add instructor list styles

--- src/app/dance-features/component/instructors/listInstructors.component.ts
+++ src/app/dance-features/component/instructors/listInstructors.component.ts
@@ -16,11 +16,15 @@ export class ListInstructorsComponent implements OnInit {
               {
                   'id': '1',
                   'name': 'Henryk Brzeziński',
+                  'image': 'assets/image/instructors/brzezinski.jpg',
+                  'activity_type' : 'Salsa',
                   'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque imperdiet risus in nunc luctus, a vulputate erat dapibus. Aliquam pulvinar vehicula pellentesque. Phasellus condimentum elit euismod ante vestibulum sollicitudin. Duis convallis facilisis diam a maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam justo diam, tempor vel faucibus eget, convallis vel sapien. Morbi luctus nisl elit, vitae vehicula urna dictum sed. Nunc scelerisque semper nisi in commodo. Curabitur semper porta dui at euismod. Aliquam risus lorem, sollicitudin a nisl sed, laoreet fermentum velit.'
               },
               {
                   'id': '2',
                   'name': 'Bartosz Cieślak',
+                  'image': 'assets/image/instructors/cieslak.jpg',
+                  'activity_type' : 'Kizomba',
                   'description': 'Nullam pretium dapibus imperdiet. Morbi sit amet dolor in enim egestas fermentum et vitae erat. Nullam auctor felis ac lacus aliquet, vel semper ex fermentum. Nulla vulputate tempor laoreet. In convallis eu turpis id egestas. Quisque in eros pellentesque, fermentum sem nec, mattis eros. Aenean commodo, sem a mattis maximus, felis lacus aliquet lectus, ut elementum ligula urna at enim. Suspendisse potenti. Nulla hendrerit turpis purus, vel euismod nisi vestibulum nec. Nam eu tristique mauris, et imperdiet elit. Donec varius justo nisl, vel euismod mi euismod id. Pellentesque vel laoreet risus. Cras pulvinar eleifend neque, eu tincidunt justo pulvinar ut.'
               }
           ]
